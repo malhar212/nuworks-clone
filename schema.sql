3f2137e672b75bc687cf41169a33abf53e4b7c29
Malhar <m2mahant12@gmail.com>
2022-04-17 19:11:09 -0400

    Initial Create Schema

---
+++ schema.sql
@@ -0,0 +1,284 @@
+CREATE DATABASE  IF NOT EXISTS `main` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci */ /*!80016 DEFAULT ENCRYPTION='N' */;
+USE `main`;
+-- MySQL dump 10.13  Distrib 8.0.28, for Win64 (x86_64)
+--
+-- Host: localhost    Database: main
+-- ------------------------------------------------------
+-- Server version	8.0.28
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!50503 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `application`
+--
+
+DROP TABLE IF EXISTS `application`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `application` (
+  `applicationid` int NOT NULL,
+  `skills` varchar(45) DEFAULT NULL,
+  `education` varchar(45) DEFAULT NULL,
+  `experience` varchar(45) DEFAULT NULL,
+  `submitdate` varchar(45) DEFAULT NULL,
+  `status` varchar(45) DEFAULT NULL,
+  `locationid` int DEFAULT NULL,
+  PRIMARY KEY (`applicationid`),
+  KEY `fk_applicationlocationid_idx` (`locationid`),
+  CONSTRAINT `fk_applicationlocationid` FOREIGN KEY (`locationid`) REFERENCES `location` (`locationid`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `application`
+--
+
+LOCK TABLES `application` WRITE;
+/*!40000 ALTER TABLE `application` DISABLE KEYS */;
+/*!40000 ALTER TABLE `application` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `city`
+--
+
+DROP TABLE IF EXISTS `city`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `city` (
+  `cityid` int NOT NULL,
+  `cityname` varchar(45) DEFAULT NULL,
+  `stateid` int DEFAULT NULL,
+  `locationid` int DEFAULT NULL,
+  PRIMARY KEY (`cityid`),
+  KEY `fk_stateid_idx` (`stateid`),
+  KEY `fk_locationid_idx` (`locationid`),
+  CONSTRAINT `fk_citylocationid` FOREIGN KEY (`locationid`) REFERENCES `location` (`locationid`),
+  CONSTRAINT `fk_citystateid` FOREIGN KEY (`stateid`) REFERENCES `state` (`stateid`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `city`
+--
+
+LOCK TABLES `city` WRITE;
+/*!40000 ALTER TABLE `city` DISABLE KEYS */;
+/*!40000 ALTER TABLE `city` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `document`
+--
+
+DROP TABLE IF EXISTS `document`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `document` (
+  `docid` int NOT NULL,
+  `docname` varchar(45) DEFAULT NULL,
+  `docurl` varchar(45) DEFAULT NULL,
+  `applicationid` int DEFAULT NULL,
+  PRIMARY KEY (`docid`),
+  KEY `fk_documentapplicationid_idx` (`applicationid`),
+  CONSTRAINT `fk_documentapplicationid` FOREIGN KEY (`applicationid`) REFERENCES `application` (`applicationid`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `document`
+--
+
+LOCK TABLES `document` WRITE;
+/*!40000 ALTER TABLE `document` DISABLE KEYS */;
+/*!40000 ALTER TABLE `document` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `job`
+--
+
+DROP TABLE IF EXISTS `job`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `job` (
+  `jobid` int NOT NULL,
+  `position` varchar(45) DEFAULT NULL,
+  `type` varchar(45) DEFAULT NULL,
+  `description` varchar(45) DEFAULT NULL,
+  `publishdate` varchar(45) DEFAULT NULL,
+  `vacancy_count` varchar(45) DEFAULT NULL,
+  `status` varchar(45) DEFAULT NULL,
+  `locationid` int DEFAULT NULL,
+  `organizationid` int DEFAULT NULL,
+  PRIMARY KEY (`jobid`),
+  KEY `fk_joblocationid_idx` (`locationid`),
+  KEY `fk_joborganizationid_idx` (`organizationid`),
+  CONSTRAINT `fk_joblocationid` FOREIGN KEY (`locationid`) REFERENCES `location` (`locationid`),
+  CONSTRAINT `fk_joborganizationid` FOREIGN KEY (`organizationid`) REFERENCES `organization` (`orgid`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `job`
+--
+
+LOCK TABLES `job` WRITE;
+/*!40000 ALTER TABLE `job` DISABLE KEYS */;
+/*!40000 ALTER TABLE `job` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `location`
+--
+
+DROP TABLE IF EXISTS `location`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `location` (
+  `locationid` int NOT NULL,
+  `locationname` varchar(45) DEFAULT NULL,
+  PRIMARY KEY (`locationid`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `location`
+--
+
+LOCK TABLES `location` WRITE;
+/*!40000 ALTER TABLE `location` DISABLE KEYS */;
+/*!40000 ALTER TABLE `location` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `organization`
+--
+
+DROP TABLE IF EXISTS `organization`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `organization` (
+  `orgid` int NOT NULL,
+  `name` varchar(45) DEFAULT NULL,
+  `category` varchar(45) DEFAULT NULL,
+  `size` varchar(45) DEFAULT NULL,
+  `description` varchar(45) DEFAULT NULL,
+  `website` varchar(45) DEFAULT NULL,
+  `locationid` int DEFAULT NULL,
+  PRIMARY KEY (`orgid`),
+  KEY `fk_organizationlocationid_idx` (`locationid`),
+  CONSTRAINT `fk_organizationlocationid` FOREIGN KEY (`locationid`) REFERENCES `location` (`locationid`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `organization`
+--
+
+LOCK TABLES `organization` WRITE;
+/*!40000 ALTER TABLE `organization` DISABLE KEYS */;
+/*!40000 ALTER TABLE `organization` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `role`
+--
+
+DROP TABLE IF EXISTS `role`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `role` (
+  `roleid` int NOT NULL AUTO_INCREMENT,
+  `rolename` varchar(45) DEFAULT NULL,
+  PRIMARY KEY (`roleid`)
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `role`
+--
+
+LOCK TABLES `role` WRITE;
+/*!40000 ALTER TABLE `role` DISABLE KEYS */;
+INSERT INTO `role` VALUES (1,'Applicant'),(2,'Recruiter');
+/*!40000 ALTER TABLE `role` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `state`
+--
+
+DROP TABLE IF EXISTS `state`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `state` (
+  `stateid` int NOT NULL,
+  `name` varchar(45) DEFAULT NULL,
+  `locationid` int DEFAULT NULL,
+  PRIMARY KEY (`stateid`),
+  KEY `fk_locationid_idx` (`locationid`),
+  CONSTRAINT `fk_locationid` FOREIGN KEY (`locationid`) REFERENCES `location` (`locationid`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `state`
+--
+
+LOCK TABLES `state` WRITE;
+/*!40000 ALTER TABLE `state` DISABLE KEYS */;
+/*!40000 ALTER TABLE `state` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `user`
+--
+
+DROP TABLE IF EXISTS `user`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `user` (
+  `user_id` int NOT NULL AUTO_INCREMENT,
+  `email` varchar(45) NOT NULL,
+  `first_name` varchar(45) DEFAULT NULL,
+  `last_name` varchar(45) DEFAULT NULL,
+  `password` varchar(45) DEFAULT NULL,
+  `bio` varchar(45) DEFAULT NULL,
+  `role_id` int DEFAULT NULL,
+  PRIMARY KEY (`email`),
+  UNIQUE KEY `user_id_UNIQUE` (`user_id`),
+  KEY `fk_roleid_idx` (`role_id`),
+  CONSTRAINT `fk_roleid` FOREIGN KEY (`role_id`) REFERENCES `role` (`roleid`)
+) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `user`
+--
+
+LOCK TABLES `user` WRITE;
+/*!40000 ALTER TABLE `user` DISABLE KEYS */;
+INSERT INTO `user` VALUES (5,'a1be2ca@gmail.com','aa2ebc','xa2ye','2abcad22123','bio1',1),(3,'ab2ca@gmail.com','aa2bc','xa2y','2abcad22123','bio1',2),(1,'abca@gmail.com','aabc','xay','abcad2123','bio1',2),(4,'abe2ca@gmail.com','aa2ebc','xa2ye','2abcad22123','bio1',2);
+/*!40000 ALTER TABLE `user` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2022-04-17 23:22:54
